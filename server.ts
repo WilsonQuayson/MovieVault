cd3ae217e3fe5454dd67cdd5143df6567a45bed8
Wilson Quayson <wilsonquayson1999@gmail.com>
2024-08-21 18:42:48 +0000

    change links int GET Requests

--- server.ts
+++ server.ts
@@ -11,8 +11,28 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+    res.render("index");
+});
+
+app.get("/series", (req, res) => {
+    res.render("series");
+});
+
+app.get("/films", (req, res) => {
+    res.render("films");
+});
+
+app.get("/list", (req, res) => {
+    res.render("mylist");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
 app.listen(app.get("port"), async () => {
     console.log(`Server is running on port ${app.get("port")}`);
